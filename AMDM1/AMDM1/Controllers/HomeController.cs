4ef66b06b404abc3a0a5c869c2e0f578c2c78270
Александр Волк <alex_v41@hotmail.com>
2016-09-16 14:05:19 +0300

    listView

--- AMDM1/AMDM1/Controllers/HomeController.cs
+++ AMDM1/AMDM1/Controllers/HomeController.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
+using System.Web.ModelBinding;
 using System.Web.Mvc;
 
 namespace AMDM1.Controllers
@@ -13,7 +14,25 @@ namespace AMDM1.Controllers
             return View();
         }
 
-        public ActionResult About()
+        public partial class ProductList : System.Web.UI.Page
+        {
+            protected void Page_Load(object sender, EventArgs e)
+            {
+            }
+            
+        public IQueryable<Performer> GetPerformers([QueryString("id")] int? categoryId)
+            {
+                var _db = new AmdmContext();
+                IQueryable<Performer> query = _db.Performers;
+                if (categoryId.HasValue && categoryId > 0)
+                {
+                    query = query.Where(p => p.Id == categoryId);
+                }
+                return query; }
+        }
+    
+
+    public ActionResult About()
         {
             ViewBag.Message = "Your application description page.";
 
