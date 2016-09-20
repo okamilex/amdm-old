345fdf2f1a5167de28bddbccedc093f869a15750
Александр Волк <alex_v41@hotmail.com>
2016-09-20 14:00:43 +0300

    ajax

--- AMDM1/AMDM1/Controllers/HomeController.cs
+++ AMDM1/AMDM1/Controllers/HomeController.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Web;
 using System.Web.ModelBinding;
 using System.Web.Mvc;
+using AMDM1.Models;
 
 namespace AMDM1.Controllers
 {
@@ -12,8 +13,10 @@ namespace AMDM1.Controllers
         public ActionResult Index()
         {
             var db = new AmdmContext();
-            var per = db.Performers;
-            return View(per);
+            var list = db.Performers.Where(x => x.Id < 11).ToList();
+            Set set = new Set { SortType = 1, Page = 1, MaxPage = list.Count / 10 };
+            PerformersModel pm = new PerformersModel { Performers = list, Set = set };            
+            return View(pm);
         }
         public ActionResult Performer(int id=0)
         {
@@ -27,6 +30,17 @@ namespace AMDM1.Controllers
             var song = db.Songs.Where(x => x.Id == id);
             return View(song.First());
             
-        }       
+        }
+        [HttpPost]
+        public ActionResult PerformersList(int type, int page)
+        {
+            var db = new AmdmContext();
+            var list = db.Performers.Where(x => ((x.Id > (page - 1) * 10) && (x.Id < page * 10 + 1))).ToList();
+            Set set = new Set { SortType = type, Page = page, MaxPage = list.Count / 10 };
+            PerformersModel pm = new PerformersModel { Performers = list, Set = set };
+            
+
+            return PartialView(pm);
+        }
     }
 }
