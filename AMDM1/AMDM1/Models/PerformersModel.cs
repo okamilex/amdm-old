46eec7c812ee179ba44691cd5aab2b0f2a3c298a
Александр Волк <alex_v41@hotmail.com>
2016-09-20 14:03:21 +0300

    ajax 2

---
+++ AMDM1/AMDM1/Models/PerformersModel.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using AMDM1.Controllers;
+
+namespace AMDM1.Models
+{
+    public class PerformersModel
+    {
+        public List<Performer> Performers { get; set; }
+        public Set Set { get; set; }
+    }
+}
---
+++ AMDM1/AMDM1/Models/Set.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace AMDM1.Models
+{
+    public class Set
+    {
+        public int SortType { get; set; }
+        public int Page { get; set; }
+        public int MaxPage { get; set; }
+    }
+}
