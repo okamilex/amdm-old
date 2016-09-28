817c6eae1b467eb87edf155b8bc9d1f8e29ed435
Александр Волк <alex_v41@hotmail.com>
2016-09-28 10:23:22 +0300

    signalR fix

--- amdm/AmdmWeb/Scripts/client-server.js
+++ amdm/AmdmWeb/Scripts/client-server.js
@@ -1,87 +1,26 @@
 ﻿$(function () {
 
-    $('#chatBody').hide();
-    $('#loginBlock').show();
-    // Ссылка на автоматически-сгенерированный прокси хаба
+   
+    
 
     debugger;
-    var chat = $.connection.amdmHub;
-    // Объявление функции, которая хаб вызывает при получении сообщений
+    var chat = $.connection.amdmHub;    
     chat.client.addMessage = function (name, message) {
         debugger;
         $('#alertDiv').append('<div class="alert alert-success">'+
                                 '<a href="#" class="close" data-dismiss="alert" aria-label="close">&times;</a>'+
                                 '<strong>' + name +'</strong>' + message + 
                               '</div>'
-        );
-
-        // Добавление сообщений на веб-страницу 
-        //$('#alertDiv').append('<p><b>' + htmlEncode(name) + '</b>: ' + htmlEncode(message) + '</p>');
+        );        
     };
-
-    // Функция, вызываемая при подключении нового пользователя
-    chat.client.onConnected = function (id, userName, allUsers) {
-
-        $('#loginBlock').hide();
-        $('#chatBody').show();
-        // установка в скрытых полях имени и id текущего пользователя
-        $('#hdId').val(id);
-        $('#username').val(userName);
-        $('#header').html('<h3>Добро пожаловать, ' + userName + '</h3>');
-
-        // Добавление всех пользователей
-        for (i = 0; i < allUsers.length; i++) {
-
-            AddUser(allUsers[i].ConnectionId, allUsers[i].Name);
-        }
-    }
-
-    // Добавляем нового пользователя
-    chat.client.onNewUserConnected = function (id, name) {
-
-        AddUser(id, name);
-    }
-
-    // Удаляем пользователя
-    chat.client.onUserDisconnected = function (id, userName) {
-
-        $('#' + id).remove();
-    }
-
-    // Открываем соединение
+  
     $.connection.hub.start().done(function () {
 
-        $('#sendmessage').click(function () {
-            // Вызываем у хаба метод Send
-            chat.server.send('test0', 'test1');
-            
-        });
+       
 
-        // обработка логина
-        $("#btnLogin").click(function () {
-
-            var name = $("#txtUserName").val();
-            if (name.length > 0) {
-                chat.server.connect(name);
-            }
-            else {
-                alert("Введите имя");
-            }
-        });
+      
+       
     });
 });
-// Кодирование тегов
-function htmlEncode(value) {
-    var encodedValue = $('<div />').text(value).html();
-    return encodedValue;
-}
-//Добавление нового пользователя
-function AddUser(id, name) {
 
-    var userId = $('#hdId').val();
 
-    if (userId != id) {
-
-        $("#chatusers").append('<p id="' + id + '"><b>' + name + '</b></p>');
-    }
-}
